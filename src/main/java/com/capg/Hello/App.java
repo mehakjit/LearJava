97527d45dc9f331e298116e6af7130ef2bc7cc11
mehakjit <mehakjitsingh@gmail.com>
2020-09-21 11:59:25 +0530

    first commit

---
+++ src/main/java/com/capg/Hello/App.java
@@ -0,0 +1,22 @@
+package com.capg.Hello;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        String message = "Hello World!";
+        LOG.debug(message + "ON LOG.DEBUG ");
+        LOG.info(message + "ON LOG.DEBUG ");
+        LOG.warn(message + "ON LOG.DEBUG ");
+        LOG.error(message + "ON LOG.DEBUG ");
+        LOG.debug("Apppending string: {}.", message);
+        LOG.fatal(message + "ON LOG.DEBUG ");
+    }
+}
